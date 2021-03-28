f15d4f12b6355d61762c959a72e7284cc47901a7
chadcomiter <chadcomiter@gmail.com>
2021-03-28 15:37:35 -0400

    Added Repo

--- src/main/java/com/example/coursemanager/models/Widget.java
+++ src/main/java/com/example/coursemanager/models/Widget.java
@@ -1,10 +1,54 @@
 package com.example.coursemanager.models;
+import javax.persistence.*;
+
+@Entity
+@Table(name="widgets")
 public class Widget{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String topicId;
     private String type;
     private Integer size;
     private String text;
+    private String name;
+    private Integer widgetOrder;
+    private String url;
+    private Integer width;
+    private Integer height;
+    private String cssClass;
+    private String style;
+    private String value;
+    private Boolean ordered;
+
+    public Widget() {
+    }
+
+    public Widget(Long id, String topicId, String type, Integer size, String text, String name, Integer widgetOrder, String url, Integer width, Integer height, String cssClass, String style, String value, Boolean ordered) {
+        this.id = id;
+        this.topicId = topicId;
+        this.type = type;
+        this.size = size;
+        this.text = text;
+        this.name = name;
+        this.widgetOrder = widgetOrder;
+        this.url = url;
+        this.width = width;
+        this.height = height;
+        this.cssClass = cssClass;
+        this.style = style;
+        this.value = value;
+        this.ordered = ordered;
+    }
+
+    public Widget(long id, String topicId, String type, Integer size, String text) {
+        this.id = id;
+        this.topicId = topicId;
+        this.type = type;
+        this.size = size;
+        this.text = text;
+    }
 
     public Long getId() {
         return this.id;
@@ -46,17 +90,78 @@ public class Widget{
         this.text = text;
     }
 
-
-    public Widget() {
+    public String getName() {
+        return this.name;
     }
 
-    public Widget(Long id, String topicId, String type, Integer size, String text) {
-        this.id = id;
-        this.topicId = topicId;
-        this.type = type;
-        this.size = size;
-        this.text = text;
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public Integer getWidgetOrder() {
+        return this.widgetOrder;
+    }
+
+    public void setWidgetOrder(Integer widgetOrder) {
+        this.widgetOrder = widgetOrder;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getCssClass() {
+        return this.cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return this.style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Boolean getOrdered() {
+        return this.ordered;
+    }
+
+    public void setOrdered(Boolean ordered) {
+        this.ordered = ordered;
+    }
+
 
 
 }
---
+++ src/main/java/com/example/coursemanager/repositories/WidgetRepository.java
@@ -0,0 +1,21 @@
+package com.example.coursemanager.repositories;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import com.example.coursemanager.models.Widget;
+
+import java.util.List;
+
+public interface WidgetRepository
+    extends CrudRepository<Widget, Long> {
+
+//    @Query(value="SELECT * FROM widgets", nativeQuery = true)
+    @Query("SELECT widget FROM Widget widget")
+    public List<Widget> findAllWidgets();
+    
+    @Query(value="SELECT * FROM widgets WHERE id=:wid", nativeQuery = true)
+    public Widget findWidgetById(@Param("wid") Long widgetId);
+    
+    @Query(value="SELECT * FROM widgets WHERE topic_id=:tid", nativeQuery = true)
+    public List<Widget> findWidgetsForTopic(@Param("tid") String topicId);
+}
